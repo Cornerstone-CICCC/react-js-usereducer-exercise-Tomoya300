e5efe336acdd8c10b705ffb89387337456f6f77f
Tomoya <flyingfox193807@gmail.com>
2025-05-21 12:01:14 -0700

    added restriction to font size

--- reducer-exercise/src/App.tsx
+++ reducer-exercise/src/App.tsx
@@ -3,11 +3,13 @@ import themeReducer from "./reducers/themeReducer"
 
 const App = () => {
   const [state, dispatch] = useReducer(themeReducer, { isDark: false, fSize: 16 })
-  
+
   return (
-    <div style={{backgroundColor: state.isDark ? 'black' : 'white', color: state.isDark ? 'white' : 'black', fontSize: state.fSize}}>
-      <h2>dark mode test</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est delectus ratione quis nihil veritatis hic eveniet, dignissimos vero? Eum quasi excepturi illum assumenda facere voluptatibus, placeat nisi beatae id exercitationem.</p>
+    <div style={{width: "100%", height: "100%"}}>
+      <div style={{backgroundColor: state.isDark ? 'black' : 'white', color: state.isDark ? 'white' : 'black', fontSize: state.fSize < 6 ? '1' : state.fSize}}>
+        <h2>dark mode test</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est delectus ratione quis nihil veritatis hic eveniet, dignissimos vero? Eum quasi excepturi illum assumenda facere voluptatibus, placeat nisi beatae id exercitationem.</p>
+      </div>
       <button onClick={() => dispatch({ type: 'toggle' })}>Toggle Dark Mode</button>
       <button onClick={() => dispatch({ type: 'increase' })}>Increase Font Size</button>
       <button onClick={() => dispatch({ type: 'decrease' })}>Decrease Font Size</button>
